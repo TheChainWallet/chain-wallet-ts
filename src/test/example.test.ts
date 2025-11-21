e1b89f9d3ba070229b045573571820af6b619722
Snail <Snail@sanil.com>
2025-11-21 23:29:23 +0800

    Revert "first"
    This reverts commit da1aac8e279ba8c70423e568119c552307a5d550.

--- src/test/example.test.ts
+++
@@ -1,9 +0,0 @@
-import {ChainWalletClient} from "../chain-wallet/chain-wallet";
-
-describe("test example", () => {
-
-    const chainWalletClient = new ChainWalletClient({})
-    it("it is a example test",async ()=>{
-        console.log("hello world")
-    })
-})
