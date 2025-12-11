1c808405fe6aebbd4034eafd6b4d2d31005bc7b5
Snail <Snail@sanil.com>
2025-12-11 10:20:27 +0800

    rebuild

--- dist/cjs/utils.js
+++ dist/cjs/utils.js
@@ -15,29 +15,15 @@ var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (
 }) : function(o, v) {
     o["default"] = v;
 });
-var __importStar = (this && this.__importStar) || (function () {
-    var ownKeys = function(o) {
-        ownKeys = Object.getOwnPropertyNames || function (o) {
-            var ar = [];
-            for (var k in o) if (Object.prototype.hasOwnProperty.call(o, k)) ar[ar.length] = k;
-            return ar;
-        };
-        return ownKeys(o);
-    };
-    return function (mod) {
-        if (mod && mod.__esModule) return mod;
-        var result = {};
-        if (mod != null) for (var k = ownKeys(mod), i = 0; i < k.length; i++) if (k[i] !== "default") __createBinding(result, mod, k[i]);
-        __setModuleDefault(result, mod);
-        return result;
-    };
-})();
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.uint8ArrayAlterFirst = void 0;
-exports.getTransactionHashWithNonce = getTransactionHashWithNonce;
-exports.signHash32 = signHash32;
-exports.replaceWith = replaceWith;
-exports.toVersionTransaction = toVersionTransaction;
+exports.toVersionTransaction = exports.replaceWith = exports.uint8ArrayAlterFirst = exports.signHash32 = exports.getTransactionHashWithNonce = void 0;
 const web3_js_1 = require("@solana/web3.js");
 const nacl = __importStar(require("tweetnacl"));
 async function getTransactionHashWithNonce(ins, skip, nonce) {
@@ -84,12 +70,14 @@ async function getTransactionHashWithNonce(ins, skip, nonce) {
     }
     return new Uint8Array(digest);
 }
+exports.getTransactionHashWithNonce = getTransactionHashWithNonce;
 function signHash32(hash, keypair) {
     if (hash.length !== 32) {
         throw new Error('Hash must be 32 bytes');
     }
     return nacl.sign.detached(hash, keypair.secretKey);
 }
+exports.signHash32 = signHash32;
 const uint8ArrayAlterFirst = (data, replaceFirst) => {
     data.set(replaceFirst, 0);
 };
@@ -102,6 +90,7 @@ function replaceWith(array, target, replacer, equalsFn) {
         }
     }
 }
+exports.replaceWith = replaceWith;
 function toVersionTransaction(tx, payer, recentBlockhash) {
     const messageV0 = new web3_js_1.TransactionMessage({
         payerKey: payer,
@@ -110,4 +99,5 @@ function toVersionTransaction(tx, payer, recentBlockhash) {
     }).compileToV0Message();
     return new web3_js_1.VersionedTransaction(messageV0);
 }
+exports.toVersionTransaction = toVersionTransaction;
 //# sourceMappingURL=utils.js.map
